63d9065d44ec588b16d508428862fb22126f5559
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-14 23:17:41 +0600

    Add initial setup for Praxleo WordPress theme with Tailwind CSS
    - Created package.json for project dependencies and scripts
    - Added input.css to import Tailwind CSS
    - Configured tailwind.config.js for content paths and theme customization

--- inc/Assets.php
+++ inc/Assets.php
@@ -4,7 +4,7 @@ namespace Praxleo;
 
 class Assets {
     public static function enqueue() {
-        wp_enqueue_style( 'praxleo-style', PRAXLEO_URI . '/style.css', array(), PRAXLEO_VERSION );
+        wp_enqueue_style( 'praxleo-style', PRAXLEO_URI . '/assets/css/style.css', array(), PRAXLEO_VERSION );
         wp_enqueue_script( 'praxleo-script', PRAXLEO_URI . '/assets/js/script.js', array(), PRAXLEO_VERSION, true );
     }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./**/*.php",
+    "./assets/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

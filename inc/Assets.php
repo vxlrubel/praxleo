2e136d49a09b93eb7faadd01861dbac8f8d066bd
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-14 23:07:06 +0600

    Add Assets class to enqueue styles and scripts

---
+++ inc/Assets.php
@@ -0,0 +1,10 @@
+<?php 
+
+namespace Praxleo;
+
+class Assets {
+    public static function enqueue() {
+        wp_enqueue_style( 'praxleo-style', PRAXLEO_URI . '/style.css', array(), PRAXLEO_VERSION );
+        wp_enqueue_script( 'praxleo-script', PRAXLEO_URI . '/assets/js/script.js', array(), PRAXLEO_VERSION, true );
+    }
+}

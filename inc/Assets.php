cb5c718b27b2575cdde24f4b68f3b788b9e50c73
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-14 23:39:02 +0600

    Reorder asset enqueuing in Assets class; move 'praxleo-style' to follow 'praxleo-custom'

--- inc/Assets.php
+++ inc/Assets.php
@@ -4,8 +4,10 @@ namespace Praxleo;
 
 class Assets {
     public static function enqueue() {
-        wp_enqueue_style( 'praxleo-style', get_stylesheet_uri(), array(), PRAXLEO_VERSION );
         wp_enqueue_style( 'praxleo-custom', PRAXLEO_URI . '/assets/css/custom.css', array(), PRAXLEO_VERSION );
+        wp_enqueue_style( 'praxleo-style', get_stylesheet_uri(), array(), PRAXLEO_VERSION );
+
+        // custom script
         wp_enqueue_script( 'praxleo-script', PRAXLEO_URI . '/assets/js/script.js', array(), PRAXLEO_VERSION, true );
     }
 }

55a11ef9264aa966343de22743ec96a7d08ee58b
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-14 23:55:14 +0600

    Enhance header layout with Tailwind CSS classes; add navigation links and styling

--- header.php
+++ header.php
@@ -9,5 +9,15 @@
 <?php wp_body_open(); ?>
     
 <header class="bg-rose-500">
-    header content
+    <div class="mx-auto max-w-350 px-5 py-4 flex">
+        <a href="<?php echo home_url(); ?>" class="text-white font-bold text-xl">Praxleo</a>
+        <ul class="ml-auto flex space-x-4">
+            <li><a href="#" class="text-white hover:text-gray-200">Home</a></li>
+            <li><a href="#" class="text-white hover:text-gray-200">About</a></li>
+            <li><a href="#" class="text-white hover:text-gray-200">Services</a></li>
+            <li><a href="#" class="text-white hover:text-gray-200">Portfolio</a></li>
+            <li><a href="#" class="text-white hover:text-gray-200">Blog</a></li>
+            <li><a href="#" class="text-white hover:text-gray-200">Contact</a></li>
+        </ul>
+    </div>
 </header>

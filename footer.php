060281c52426f5219ab433dfa521ef8520a60e7b
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-15 00:41:28 +0600

    Add custom rose color and update footer styling

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
 </main>
-<footer>footer content</footer>
+<footer class="bg-rose-50">
+    <div class="mx-auto max-w-350 px-5 py-4 flex">
+        footer content
+    </div>
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>

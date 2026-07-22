e24c51313b56ac4e315a6705f3eacb263317ffd7
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-23 02:02:31 +0600

    Refactor code structure for improved readability and maintainability

--- templates/home.php
+++ templates/home.php
@@ -9,8 +9,28 @@
 get_header();?>
 
 <section>
-    <div class="bg-linear-to-r from-theme-100 to-purple-100 min-h-75 md:min-h-85 lg:min-h-115 flex items-center justify-center px-5 pb-5">
-        <h1 class="text-gradient text-[2rem] lg:text-[3rem] font-semibold pt-20">Home</h1>
+    <div class="bg-linear-to-r from-purple-100 to-theme-100 min-h-75 md:min-h-85 lg:min-h-115 flex items-center justify-center pb-15 pt-35">
+        <div class="w-full flex-1 flex items-center container g-5 flex-col md:flex-row lg:gap-10">
+            <div class="flex-1 space-y-5 order-2 md:order-1">
+                <h1 class="text-gradient text-[1.5rem] sm:text-[2rem] lg:text-[3rem] font-semibold text-balance leading-[2rem] sm:leading-[2.5rem] lg:leading-[4rem]">
+                    Transforming Ideas into Digital Solutions
+                </h1>
+                <p>
+                    At Praxleo, we specialize in providing top-notch web design, development, and other digital services to enhance your business growth and online presence.
+                </p>
+
+                <div class="flex gap-5 flex-wrap">
+                    <a href="<?php echo esc_url(home_url('/contact')); ?>" class="button-primary">Get Started</a>
+                    <a href="<?php echo esc_url(home_url('/services')); ?>" class="button-outline-primary">Our Services</a>
+                </div>
+            </div>
+            <div class="flex-1 order-1 md:order-2">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/home-hero-image.webp" fetchpriority="high" alt="Home" class="w-full max-w-[500px] block mx-auto mix-blend-multiply">
+            </div>
+            
+
+        </div>
+        
     </div>
     
     <div>

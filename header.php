9e52afeca2e00b6f5fa330bfbfaadd08efe19a40
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-23 00:51:13 +0600

    feat: Update header with logo and adjust z-index; add new assets
    - Changed header z-index from 50 to 30 for better layering.
    - Replaced text logo with an image logo in the header.
    - Updated offcanvas menu to include logo and adjusted overlay background.
    - Added new dashboard logo and favicon images in WEBP format.
    - Introduced a new PNG logo asset for branding.

--- header.php
+++ header.php
@@ -8,9 +8,11 @@
 <body class="<?php body_class();?>">
 <?php wp_body_open(); ?>
     
-<header class="fixed top-8 left-0 right-0 z-50 px-5">
+<header class="fixed top-8 left-0 right-0 z-30 px-5">
     <div class="mx-auto max-w-350 px-5 py-4 flex items-center bg-linear-to-r from-purple-100 to-theme-100 rounded-[100px] shadow-lg">
-        <a href="<?php echo home_url(); ?>" class="text-gradient font-bold text-xl">Praxleo</a>
+        <a href="<?php echo home_url(); ?>" class="text-gradient font-bold text-xl">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="praxleo logo" class="w-30 inline-block mr-2">
+        </a>
 
         <?php if ( has_nav_menu( 'primary' ) ) : ?>
             <nav class="ml-auto hidden lg:flex space-x-4">
@@ -36,11 +38,13 @@
 </header>
 
 <?php if ( has_nav_menu( 'secondary' ) ) : ?>
-    <div id="offcanvas-overlay" class="fixed inset-0 bg-black/50 z-40 hidden"></div>
-    <aside id="offcanvas-menu" class="fixed top-0 left-0 h-full w-72 bg-linear-to-r from-purple-100 to-theme-100 z-50 transform -translate-x-full transition-transform duration-300 shadow-xl">
-        <div class="flex items-center justify-between px-5 py-4 border-b">
-            <span class="font-bold text-lg">Menu</span>
-            <button id="menu-close" class="text-gray-600 hover:text-black focus:outline-none" aria-label="Close menu">
+    <div id="offcanvas-overlay" class="fixed inset-0 bg-black/5 z-40 hidden"></div>
+    <aside id="offcanvas-menu" class="fixed top-0 left-0 h-full w-72 bg-white/10  backdrop-blur-[5px] z-50 transform -translate-x-full transition-transform duration-300 shadow-xl">
+        <div class="flex items-center justify-between px-5 py-4 border-b border-purple-300">
+            <a href="<?php echo home_url(); ?>" class="font-bold text-lg">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="praxleo logo" class="w-30 inline-block mr-2">
+            </a>
+            <button id="menu-close" class="text-purple-500 hover:text-black focus:outline-none" aria-label="Close menu">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
                 </svg>
